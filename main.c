e087db4d0d8174c472b09648d1615e6f85c4b94a
ChenTianyi-0311 <80047128+ChenTianyi-0311@users.noreply.github.com>
2021-05-05 22:12:36 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,32 +4,32 @@ unsigned int capt;
 unsigned int diff_capt;
 void main(void)
 {
-  WDTCTL = WDTPW + WDTHOLD;
-  BCSCTL1 = CALBC1_1MHZ;
-  DCOCTL = CALDCO_1MHZ;
-  TA0CTL |= TASSEL_2 + MC_2; // source horloge SMCLK + mode continu
-  TA0CCTL0 |= CM_1 + CCIS_0; // front montant + CCI0A
-  TA0CCTL0 |= CAP + CCIE; // mode capture + autorisation interruption
-  P1DIR |= BIT0; // P1.0 (LED rouge) en sortie
-  P1DIR &= ~BIT1; // P1.1 en entree
-  P1SEL |= BIT1; // fonction entree capture sur P1.1 (TA0.CCI0A)
-  P1SEL2 &= ~BIT1; // fonction entree capture sur P1.1 (TA0.CCI0A)
-  __enable_interrupt();
-  while(1);
+    WDTCTL = WDTPW + WDTHOLD;
+    BCSCTL1 = CALBC1_1MHZ;
+    DCOCTL = CALDCO_1MHZ;
+    TA0CTL |= TASSEL_2 + MC_2; // source horloge SMCLK + mode continu
+    TA0CCTL0 |= CM_1 + CCIS_0; // front montant + CCI0A
+    TA0CCTL0 |= CAP + CCIE; // mode capture + autorisation interruption
+    P1DIR |= BIT0; // P1.0 (LED rouge) en sortie
+    P1DIR &= ~BIT1; // P1.1 en entree
+    P1SEL |= BIT1; // fonction entree capture sur P1.1 (TA0.CCI0A)
+    P1SEL2 &= ~BIT1; // fonction entree capture sur P1.1 (TA0.CCI0A)
+    __enable_interrupt();
+    while(1);
 }
 
 #pragma vector=TIMER0_A0_VECTOR
 __interrupt void ma_capture(void)
 {
-  capt = TA0CCR0;
-  diff_capt = capt - memo_capt;
-  memo_capt = capt;
-  if(diff_capt > 100)
-  {
-  P1OUT |= BIT0;
-  }
-  else
-  {
-  P1OUT &= ~BIT0;
-  }
+    capt = TA0CCR0;
+    diff_capt = capt - memo_capt;
+    memo_capt = capt;
+    if(diff_capt > 100)
+    {
+        P1OUT |= BIT0;
+    }
+    else
+    {
+        P1OUT &= ~BIT0;
+    }
 }
